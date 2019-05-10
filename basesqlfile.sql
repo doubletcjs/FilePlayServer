b26303e36b135625711a99273ff566ec82a194a0
doubletcjs <doubletcjs@gmail.com>
2019-05-10 17:26:14 +0800

    修改数据库脚本

--- basesqlfile.sql
+++ basesqlfile.sql
@@ -1,27 +1,30 @@
--- MySQL dump 10.13  Distrib 5.7.25, for osx10.14 (x86_64)
---
--- Host: localhost    Database: fileplay
--- ------------------------------------------------------
--- Server version	5.7.25
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 106.12.107.176 (MySQL 5.7.26-0ubuntu0.16.04.1)
+# Database: FilePlay
+# Generation Time: 2019-05-10 09:24:57 +0000
+# ************************************************************
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `account_table`
---
+
+# Dump of table account_table
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `account_table`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `account_table` (
   `userId` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `nickname` varchar(50) NOT NULL DEFAULT '',
@@ -32,49 +35,29 @@ CREATE TABLE `account_table` (
   `introduce` varchar(512) NOT NULL DEFAULT '',
   `password` varchar(100) NOT NULL DEFAULT '',
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `account_table`
---
 
-LOCK TABLES `account_table` WRITE;
-/*!40000 ALTER TABLE `account_table` DISABLE KEYS */;
-/*!40000 ALTER TABLE `account_table` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `attention_fan`
---
+# Dump of table attention_fan
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `attention_fan`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `attention_fan` (
   `objectId` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `authorId` int(20) unsigned NOT NULL,
   `userId` int(20) unsigned NOT NULL,
   PRIMARY KEY (`objectId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `attention_fan`
---
 
-LOCK TABLES `attention_fan` WRITE;
-/*!40000 ALTER TABLE `attention_fan` DISABLE KEYS */;
-/*!40000 ALTER TABLE `attention_fan` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `comment_table`
---
+# Dump of table comment_table
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `comment_table`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `comment_table` (
   `objectId` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `authorId` int(20) unsigned NOT NULL,
@@ -83,25 +66,15 @@ CREATE TABLE `comment_table` (
   `replyId` int(20) unsigned NOT NULL,
   `dynamicId` int(20) unsigned NOT NULL,
   PRIMARY KEY (`objectId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `comment_table`
---
 
-LOCK TABLES `comment_table` WRITE;
-/*!40000 ALTER TABLE `comment_table` DISABLE KEYS */;
-/*!40000 ALTER TABLE `comment_table` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `dynamic_table`
---
+# Dump of table dynamic_table
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `dynamic_table`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `dynamic_table` (
   `objectId` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `authorId` int(20) unsigned NOT NULL,
@@ -111,25 +84,15 @@ CREATE TABLE `dynamic_table` (
   `postDate` datetime NOT NULL,
   `movieId` int(20) unsigned NOT NULL,
   PRIMARY KEY (`objectId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `dynamic_table`
---
 
-LOCK TABLES `dynamic_table` WRITE;
-/*!40000 ALTER TABLE `dynamic_table` DISABLE KEYS */;
-/*!40000 ALTER TABLE `dynamic_table` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `movie_table`
---
+# Dump of table movie_table
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `movie_table`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `movie_table` (
   `movieId` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `tmdbmovieId` int(20) unsigned NOT NULL,
@@ -142,193 +105,112 @@ CREATE TABLE `movie_table` (
   `genreids` varchar(255) DEFAULT '',
   `isEpisode` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`movieId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `movie_table`
---
 
-LOCK TABLES `movie_table` WRITE;
-/*!40000 ALTER TABLE `movie_table` DISABLE KEYS */;
-/*!40000 ALTER TABLE `movie_table` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `praise_comment`
---
+# Dump of table praise_comment
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `praise_comment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `praise_comment` (
   `objectId` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `commentId` int(20) unsigned NOT NULL,
   `authorId` int(20) unsigned NOT NULL,
   PRIMARY KEY (`objectId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `praise_comment`
---
 
-LOCK TABLES `praise_comment` WRITE;
-/*!40000 ALTER TABLE `praise_comment` DISABLE KEYS */;
-/*!40000 ALTER TABLE `praise_comment` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `praise_dynamic`
---
+# Dump of table praise_dynamic
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `praise_dynamic`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `praise_dynamic` (
   `objectId` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `dynamicId` int(20) unsigned NOT NULL,
   `authorId` int(20) unsigned NOT NULL,
   PRIMARY KEY (`objectId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `praise_dynamic`
---
 
-LOCK TABLES `praise_dynamic` WRITE;
-/*!40000 ALTER TABLE `praise_dynamic` DISABLE KEYS */;
-/*!40000 ALTER TABLE `praise_dynamic` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `report_comment`
---
+# Dump of table report_comment
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `report_comment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `report_comment` (
   `objectId` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `commentId` int(20) unsigned NOT NULL,
   `authorId` int(20) unsigned NOT NULL,
   PRIMARY KEY (`objectId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `report_comment`
---
 
-LOCK TABLES `report_comment` WRITE;
-/*!40000 ALTER TABLE `report_comment` DISABLE KEYS */;
-/*!40000 ALTER TABLE `report_comment` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `report_dynamic`
---
+# Dump of table report_dynamic
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `report_dynamic`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `report_dynamic` (
   `objectId` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `dynamicId` int(20) unsigned NOT NULL,
   `authorId` int(20) unsigned NOT NULL,
   PRIMARY KEY (`objectId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `report_dynamic`
---
 
-LOCK TABLES `report_dynamic` WRITE;
-/*!40000 ALTER TABLE `report_dynamic` DISABLE KEYS */;
-/*!40000 ALTER TABLE `report_dynamic` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `report_user`
---
+# Dump of table report_user
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `report_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `report_user` (
   `objectId` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `authorId` int(20) unsigned NOT NULL,
   `userId` int(20) unsigned NOT NULL,
   PRIMARY KEY (`objectId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `report_user`
---
 
-LOCK TABLES `report_user` WRITE;
-/*!40000 ALTER TABLE `report_user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `report_user` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `want_movie`
---
+# Dump of table want_movie
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `want_movie`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `want_movie` (
   `objectId` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `movieId` int(20) unsigned NOT NULL,
   `userId` int(20) unsigned NOT NULL,
   PRIMARY KEY (`objectId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `want_movie`
---
 
-LOCK TABLES `want_movie` WRITE;
-/*!40000 ALTER TABLE `want_movie` DISABLE KEYS */;
-/*!40000 ALTER TABLE `want_movie` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `watch_movie`
---
+# Dump of table watch_movie
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `watch_movie`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `watch_movie` (
   `objectId` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `movieId` int(20) unsigned NOT NULL,
   `userId` int(20) unsigned NOT NULL,
   PRIMARY KEY (`objectId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `watch_movie`
---
 
-LOCK TABLES `watch_movie` WRITE;
-/*!40000 ALTER TABLE `watch_movie` DISABLE KEYS */;
-/*!40000 ALTER TABLE `watch_movie` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-05-10 16:24:41
